ed3bc3b4b75bd4b44f5447996ec205421db2c6ad
Michael Albonetti <malbo24@gmail.com>
2016-10-18 13:11:14 -0500

    add readme, init firebase and angular fire

---
+++ www/js/firebaseInit.js
@@ -0,0 +1,10 @@
+
+      // Initialize Firebase
+      var config = {
+        apiKey: "AIzaSyBO5hZN2eJf93-tVyMyRrwyMTby8IdPghY",
+        authDomain: "groupchat-c064f.firebaseapp.com",
+        databaseURL: "https://groupchat-c064f.firebaseio.com",
+        storageBucket: "groupchat-c064f.appspot.com",
+        messagingSenderId: "483661967604"
+      };
+      firebase.initializeApp(config);
